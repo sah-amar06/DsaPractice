9d5004b46a8d4c49451d0f5535c06deadd81f07b
Amarnath <amarnathsah2013@gmail.com>
2026-06-03 23:26:29 +0530

    Arrays Introduction

---
+++ src/arrays/ArrayIntro.java
@@ -0,0 +1,36 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class ArrayIntro {
+
+	public static void main(String[] args) {
+		
+		//Array Declaration 
+		
+		int[] num;
+		
+		//System.out.println(num.length);  //Compile time error
+		
+		//Declaration and Initialization 
+		
+		String[] name = new String[5];
+		System.out.println(name.length);
+		System.out.println(name[1]); //null 
+		
+		name[0] = "Ram";
+		name[1] = "Sita";
+		name[2] = "Mohan";
+		name[3] = "RS";
+		name[4] = " ";
+		
+		System.out.println(Arrays.toString(name));
+		
+		for(String names:name) {
+			System.out.println(names);
+		}
+		
+
+	}
+
+}

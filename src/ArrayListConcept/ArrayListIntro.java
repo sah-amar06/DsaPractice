06bec7bdca3178c0f55f3d96ac5b4c2111f6a303
Amarnath <amarnathsah2013@gmail.com>
2026-06-08 01:52:47 +0530

    ArrayList Introduction

---
+++ src/ArrayListConcept/ArrayListIntro.java
@@ -0,0 +1,28 @@
+package ArrayListConcept;
+
+import java.util.ArrayList;
+public class ArrayListIntro {
+
+	public static void main(String[] args) {
+		
+		
+		ArrayList al = new ArrayList();
+		
+		//Adding elemnet to an ArrayList
+		
+		al.add(10);
+		al.add("Ram");
+		al.add('A');
+		al.add(10.55);
+		
+		System.out.println(al.get(0));
+		System.out.println(al.get(1));
+		System.out.println(al.get(2));
+		System.out.println(al.get(3));
+	
+		
+		
+
+	}
+
+}

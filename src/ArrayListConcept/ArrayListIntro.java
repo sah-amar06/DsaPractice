7caa568f429054d648bd974674f1f6346c972fa4
Amarnath <amarnathsah2013@gmail.com>
2026-06-08 03:29:41 +0530

    Added code for MaxInRage

--- src/ArrayListConcept/ArrayListIntro.java
+++ src/ArrayListConcept/ArrayListIntro.java
@@ -14,15 +14,27 @@ public class ArrayListIntro {
 		al.add("Ram");
 		al.add('A');
 		al.add(10.55);
+		al.add(true);
+		
+		System.out.println(al.size());
 		
 		System.out.println(al.get(0));
 		System.out.println(al.get(1));
 		System.out.println(al.get(2));
 		System.out.println(al.get(3));
-	
+		System.out.println(al.get(4));
 		
+		System.out.println(al);
 		
-
+		for(int index = 0; index<al.size(); index++) {
+			System.out.println(al.get(index));
+		}
+		
+		System.out.println("-------------");
+		
+		for(Object e: al) {
+			System.out.println(e);
+		}
 	}
 
 }
--- src/arrays/MaxInArray.java
+++ src/arrays/MaxInArray.java
@@ -6,6 +6,7 @@ public class MaxInArray {
 		int[] arr = {22,56,11,90, 12};
 		
 		System.out.println(findMax(arr));
+		System.out.println(findMaxInRange(arr, 1, 4));
 
 	}
 	
@@ -21,6 +22,26 @@ public class MaxInArray {
 			if(arr[index]>max) {
 				max = arr[index];
 			}
+		}
+		return max;
+	}
+	
+	public static int findMaxInRange(int[] arr, int start, int end) {
+		
+		if (arr == null || arr.length == 0) {
+		    return -1;
+		}
+		
+		if (start < 0 || end >= arr.length || start > end) {
+		    return -1;
+		}
+		
+		int max = arr[start];
+		for(int index = start+1; index<=end; index++) {
+			if(arr[index] > max) {
+				max = arr[index];
+			}
+			
 		}
 		return max;
 	}

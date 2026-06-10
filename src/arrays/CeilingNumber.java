170c3a44aa861a28a09eb6d04a03200ee2449302
Amarnath <amarnathsah2013@gmail.com>
2026-06-10 23:47:36 +0530

    Ceiling number code for descending number

--- src/arrays/CeilingNumber.java
+++ src/arrays/CeilingNumber.java
@@ -6,17 +6,22 @@ public class CeilingNumber {
 		
 		int[] nums = {1,5,9,11,16,19,22,30,};
 		int[] nums1 = {90,80,70,60,50,40,30,20,10};
-		System.out.println(getCeilingNum(nums, 17));
+		System.out.println(getCeilingNumAscending(nums, 17));
 		System.out.println(orderCheck(nums));
 		System.out.println(orderCheck(nums1));
+		System.out.println(getCeilingNumDesc(nums1, 45));
 	}
 
-	public static int getCeilingNum(int[] arr, int target) {
+	public static int getCeilingNumAscending(int[] arr, int target) {
 
 		if (arr == null || arr.length == 0) {
 			return -1;
 		}
 
+		/**
+		 * If the target is greater than the value of the last index in as ascending order
+		 * return -1;
+		 */
 		if (target > arr[arr.length - 1]) {
 			return -1;
 		}
@@ -41,6 +46,39 @@ public class CeilingNumber {
 		}
 
 		return start;
+	}
+	
+	public static int getCeilingNumDesc(int[] arr, int target) {
+		
+		if(arr == null || arr.length==0) {
+			return -1;
+		}
+		/**
+		 * If the target is smaller than the value of the last index in the ascending order
+		 * return -1
+		 */
+		if(target< arr[arr.length-1]) {
+			return -1;
+		}
+		
+		int start = 0;
+		int end = arr.length-1;
+		
+		while(start<=end) {
+			int mid = start + (end-start)/2;
+			
+			if(arr[mid] ==target) {
+				return mid;
+			}
+			
+			if(arr[mid] > target) {
+				start = mid+1;
+			}else {
+				end = mid-1;
+			}
+		}
+		
+		return end;
 	}
 
 	public static boolean orderCheck(int[] arr) {

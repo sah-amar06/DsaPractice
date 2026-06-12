5c5082cf5300c14569a96d1da38bbec3c0409143
Amarnath <amarnathsah2013@gmail.com>
2026-06-13 02:09:50 +0530

    Code pratice for First and Last Occerence with Binary Search

--- src/arrays/LastOccurance.java
+++ src/arrays/LastOccurance.java
@@ -4,9 +4,10 @@ public class LastOccurance {
 
 	public static void main(String[] args) {
 
-		int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10, 11 };
+		int[] nums = { 1, 2, 3, 4, 5, 5, 5, 5, 6, 7, 5, 8, 9, 9, 10, 11 };
 		System.out.println(getLastOccurance(nums, 9));
 		System.out.println(getLastOccurance(nums, 15));
+		System.out.println(getLastOccuranceBS(nums, 5));
 
 	}
 
@@ -29,6 +30,35 @@ public class LastOccurance {
 
 		}
 		return last;
+	}
+	
+	public static int getLastOccuranceBS(int[] arr, int target) {
+		
+		if(arr == null || arr.length==0) {
+			return -1;
+		}
+		
+		if(target < arr[0]   || target>arr[arr.length-1]) {
+			return -1;
+		}
+		
+		int answer = -1;
+		int start = 0, end = arr.length-1;
+		
+		while(start<=end) {
+			
+			int mid = start + (end-start)/2;
+			
+			if(arr[mid] == target) {
+				answer = mid;
+				start = mid+1;	
+			}else if(arr[mid] < target) {  
+				start = mid+1;
+			}else {
+				end = mid-1;
+			}
+		}
+		return answer;
 	}
 
 }

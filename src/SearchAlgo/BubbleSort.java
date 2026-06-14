bda5e02365fa20f09003ecc450f8339507de03e7
Amarnath <amarnathsah2013@gmail.com>
2026-06-14 23:36:05 +0530

    BubbleSort algo pratice

---
+++ src/SearchAlgo/BubbleSort.java
@@ -0,0 +1,10 @@
+package SearchAlgo;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/SearchAlgo/InsertionSort.java
@@ -0,0 +1,5 @@
+package SearchAlgo;
+
+public class InsertionSort {
+
+}

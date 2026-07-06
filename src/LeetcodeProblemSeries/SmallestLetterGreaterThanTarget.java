4e8c729802a3fd41862f23887133ef9f29c6e487
Amarnath <amarnathsah2013@gmail.com>
2026-07-06 23:39:58 +0530

    Comments added

--- src/LeetcodeProblemSeries/SmallestLetterGreaterThanTarget.java
+++ src/LeetcodeProblemSeries/SmallestLetterGreaterThanTarget.java
@@ -20,11 +20,25 @@ public class SmallestLetterGreaterThanTarget {
 			int mid = start+(end-start)/2;
 			
 			if(letters[mid]>target) {
+				// letters[mid] is a possible answer because it is greater than target.
+	            // However, there might be another letter on the left that is also greater
+	            // than the target but smaller than letters[mid].
+	            // So, continue searching on the left.
 				end = mid-1;
 			}else {
+				 // letters[mid] <= target
+	            // This letter cannot be the answer because we need a strictly greater letter.
+	            // Search in the right half.
 				start = mid+1;
 			}
 		}
+		// After the loop:
+	    // 'start' points to the smallest letter greater than the target.
+	    //
+	    // If the target is greater than or equal to every letter in the array,
+	    // start becomes letters.length.
+	    //
+	    // Using modulo wraps the index back to 0 because the array is circular.
 		return letters[start % letters.length];
 	}
 

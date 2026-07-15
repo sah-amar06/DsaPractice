cd370c4a6c0c0641095ca86debfcbc90bd2fb4db
Amarnath <amarnathsah2013@gmail.com>
2026-07-16 03:19:38 +0530

    Code pratice for Bubble sort for the 3rd largest number

--- src/LeetCodeSortingProblems/ThirdMaximumNumber.java
+++ src/LeetCodeSortingProblems/ThirdMaximumNumber.java
@@ -5,8 +5,10 @@ public class ThirdMaximumNumber {
 	
 	//https://leetcode.com/problems/third-maximum-number/
 	public static void main(String[] args) {
-		int[] nums = {2,2,3,1};
+		int[] nums = {7,5};
+		int[] num1 = {2,5,6,7,9,1,11};
 		System.out.println(getThirdMaximumNumber(nums));
+		System.out.println(getThirdMaximumNumber(num1));
 
 	}
 	
@@ -14,17 +16,26 @@ public class ThirdMaximumNumber {
 		
 		
 		for(int i=0; i<nums.length; i++) {
+			
+			boolean swapped = false;
 			for(int j=1; j<nums.length-i; j++) {
 				if(nums[j]<nums[j-1]) {
 					int temp = nums[j];
 					nums[j] = nums[j-1];
 					nums[j-1] = temp;
+					swapped = true;
 				}
-				
+		
+			}
+			if(!swapped) {
+				break;
 			}
 				
 		}
 		int count =1;
+		//Here, we will get the largetest element of the array. 
+		//Since array is already sorted. that's why the last element will be largest element of the array.
+		//If count!=3, then we will get the largest element of the array as an output.
 		int max = nums[nums.length-1];
 		for(int k=nums.length-2; k>=0; k--) {
 			if(nums[k]!=nums[k+1]) {

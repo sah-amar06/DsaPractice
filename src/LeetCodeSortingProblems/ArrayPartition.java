81a36122d9fc2e78df265353982f8bde0aacf2da
Amarnath <amarnathsah2013@gmail.com>
2026-07-18 23:59:21 +0530

    Code pratice

--- src/LeetCodeSortingProblems/ArrayPartition.java
+++ src/LeetCodeSortingProblems/ArrayPartition.java
@@ -2,9 +2,37 @@ package LeetCodeSortingProblems;
 
 public class ArrayPartition {
 
+	
+	//https://leetcode.com/problems/array-partition/
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] num = {6,2,6,5,1,2};
+		System.out.println(getArrayPartitining(num));
 
+	}
+	
+	
+	public static int getArrayPartitining(int[] arr) {
+		
+		for(int i=0; i<arr.length; i++) {
+			
+			boolean swapped = false;
+			for(int j=1; j<arr.length-i; j++) {
+				if(arr[j]<arr[j-1]) {
+					int temp = arr[j];
+					arr[j]= arr[j-1];
+					arr[j-1]= temp;
+					swapped = true;
+				}
+			}
+			if(!swapped) {
+				break;
+			}
+		}
+		int sum =0;
+		for(int k=0; k<arr.length; k+=2) {
+			sum= sum+arr[k];
+		}
+		return sum;
 	}
 
 }
--- src/LeetCodeSortingProblems/DutchNationalFlagAlgo.java
+++ src/LeetCodeSortingProblems/DutchNationalFlagAlgo.java
@@ -3,8 +3,30 @@ package LeetCodeSortingProblems;
 public class DutchNationalFlagAlgo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+	}
+	
+	public static void sortElements(int[] arr) {
+		
+		int low=0, mid=0, high=arr.length-1;
+		
+		while(mid<=high) {
+			if(arr[mid]==0) {
+				int temp = arr[mid];
+				arr[mid]= arr[low];
+				arr[low] = temp;
+				low++;
+				mid++;
+			}else if(arr[mid]==1) {
+				mid++;
+			}else {
+				int temp1 = arr[mid];
+				arr[mid]= arr[high];
+				arr[high]= temp1;
+				high--;
+			}
+		}
 	}
 
 }

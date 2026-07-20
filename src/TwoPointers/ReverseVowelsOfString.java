cd02dd72324e1e8cce051b1a0cd38c84a5884a61
Amarnath <amarnathsah2013@gmail.com>
2026-07-20 23:36:16 +0530

    updated the swap method

--- src/TwoPointers/ReverseVowelsOfString.java
+++ src/TwoPointers/ReverseVowelsOfString.java
@@ -11,13 +11,10 @@ public class ReverseVowelsOfString {
 	}
 	
 	
-	/**
-	 * Two pointer technique
-	 * When both pointer have vowels, swap them 
-	 * if left have vowel and right is not a valid vowel, the right--(until a new vowel found)
-	 * if left is not a valid vowel, left++(until a new vowel)
-	 * @param str
-	 */
+	// Move left pointer until it points to a vowel
+	// Move right pointer until it points to a vowel
+	// Swap the vowels
+	// Move both pointers inward
 	public static String reverseVowels(String str) {
 		
 		char[] chars = str.toCharArray();
@@ -26,17 +23,17 @@ public class ReverseVowelsOfString {
 		int right= chars.length-1;
 		
 		while(left<right) {
-			
+			//if left is not a valid vowel, left++(until a new vowel)
 			while(left<right && !isVowel(chars[left])) {
 				left++;
 			}
+			//if left have vowel and right is not a valid vowel, the right--(until a new vowel found)
 			while(left<right && !isVowel(chars[right])) {
 				right--;
 			}
 			
-			char temp = chars[left];
-			chars[left] = chars[right];
-			chars[right] = temp;
+			//When both pointer have vowels, swap them 
+			swap(chars, left, right);
 			left++;
 			right--;
 		}
@@ -49,6 +46,12 @@ public class ReverseVowelsOfString {
 		           ch == 'o' || ch == 'u' ||
 		           ch == 'A' || ch == 'E' || ch == 'I' ||
 		           ch == 'O' || ch == 'U';
+	}
+	
+	public static void swap(char[] arr, int left, int right) {
+		char temp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = temp;
 	}
 
 }

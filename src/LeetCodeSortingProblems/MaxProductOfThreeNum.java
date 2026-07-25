1ef5cce5eebfd3e0d7e40824e6eeb18d8da22d75
Amarnath <amarnathsah2013@gmail.com>
2026-07-25 16:10:37 +0530

    Code pratice for max product of three numbers with Integer.MIN_VALUE

--- src/LeetCodeSortingProblems/MaxProductOfThreeNum.java
+++ src/LeetCodeSortingProblems/MaxProductOfThreeNum.java
@@ -7,9 +7,10 @@ public class MaxProductOfThreeNum {
 		int[] nums = {-10,-20, -30, 1,3,5,7,15,55};
 		int[] num1 = {1,2,3,6,9,11,10};
 		int[] num2 = {2,3,4};
-		System.out.println(getMaxProductOfThreeNum(nums));
-		System.out.println(getMaxProductOfThreeNum(num1));
-		System.out.println(getMaxProductOfThreeNum(num2));
+//		System.out.println(getMaxProductOfThreeNum(nums));
+//		System.out.println(getMaxProductOfThreeNum(num1));
+//		System.out.println(getMaxProductOfThreeNum(num2));
+		System.out.println(getMaxProduct(nums));
 		
 		
 
@@ -35,8 +36,50 @@ public class MaxProductOfThreeNum {
 		int n = arr.length;
 		//Product of three largest number
 		int product1 = arr[n-1]*arr[n-2]*arr[n-3];
-		//Product of two smallest and one largest number.
+		//Product of two smallest(negative) and one largest number.
 		int product2 = arr[0]*arr[1]*arr[n-1];
+
 		return Math.max(product1, product2);
 	}
+	
+	public static int getMaxProduct(int[] nums) {
+
+        // Three largest numbers
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+
+        // Two smallest numbers
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+
+            // Update three largest numbers
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (num > max3) {
+                max3 = num;
+            }
+
+            // Update two smallest numbers
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
+        }
+
+        int product1 = max1 * max2 * max3;
+        int product2 = min1 * min2 * max1;
+
+        return Math.max(product1, product2);
+    }
+
 }
